78926cfef5ea5e3e7d98fdbecc3a24d6d478ddda
hidyverse <heidiesteiner@arizona.edu>
2023-04-03 16:46:08 -0700

    update apr 06 script

--- Workshops/Spring2023/2023-Apr-06/phyloseq.R
+++ Workshops/Spring2023/2023-Apr-06/phyloseq.R
@@ -8,13 +8,9 @@ devtools::install_github("twbattaglia/MicrobeDS")
 devtools::install_github("joey711/phyloseq")
 
 ### library
-# library(metaphlanr) # for parsing taxonomic data
 library(MicrobeDS) # 
-# library(microbiome) # transforming phyloseq data
 library(phyloseq) # for metagenomic analyses
-# library(scales) # formatting numeric output
 library(tidyverse) # read data
-# library(vegan) # alpha div
 
 ### pull RISK_CCFA data from MicrobeDS
 ### doi: 10.1016/j.chom.2014.02.005
@@ -25,6 +21,33 @@ outcome <- sample_data(RISK_CCFA)
 otu <- otu_table(RISK_CCFA)
 taxa <- tax_table(RISK_CCFA)
 
-physeq <- phyloseq(otu, taxa)
-plot_bar(physeq, fill = 'Family')
+### inspect data objects
+colnames(sample_data(outcome))
+otu_table(otu)[1:5, 1:5]
+tax_table(taxa)[1:5, 1:4]
 
+### create phyloseq object
+physeq <- phyloseq(otu, taxa, outcome)
+
+ntaxa(physeq)
+nsamples(physeq)
+sample_names(physeq)[1:10]
+rank_names(physeq)
+
+### filter data
+physeqRel  = transform_sample_counts(physeq, function(x) x / sum(x) ) # relative counts
+physeqRelFilter = filter_taxa(physeqRel, function(x) mean(x) > 1e-3, TRUE) # only OTUs greater than 10^-5
+ntaxa(physeqRelFilter)
+
+### subset data
+physeqRelFilterBac = subset_taxa(physeqRelFilter, Phylum=="Bacteroidetes")
+ntaxa(physeqRelFilterBac)
+
+physeqRelFilterBacCD = physeqRelFilterBac |> 
+  subset_samples(sample_type == "stool" &  # select only stool samples
+                   diagnosis == "CD" & # from patients with crohn's
+                   smoking %in% c("Current", "Former")) # who have history of smoking
+
+### visualize data
+plot_bar(physeqRelFilterBacCD, fill = "Genus") # relative abundance of OTUs color by Genus
+plot_bar(physeqRelFilterBacCD, "Family", fill="Genus", facet_grid=~steroids) # use visualizations to summarize differences 
